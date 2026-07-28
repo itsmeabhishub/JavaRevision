18fd3d25607717a870a87a78a905e702ff759dc3
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-07-29 02:49:01 +0530

    Doing Revision of For Loop

---
+++ src/main/java/org/example/ForLoop.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=5; i++){
+            System.out.println("Days " + i);
+            for(int j = 1; j<= 9; j++){
+                System.out.println("       "  +(j+8) + " - " +(j+9));
+            }
+        }
+    }
+}
