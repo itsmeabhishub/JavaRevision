b02ec8ba09a1d6e1d34e34eb0e4b85cc7ebc22ef
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-07-28 22:55:36 +0530

    Ternary operator

---
+++ src/main/java/org/example/TernaryOperator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class TernaryOperator {
+    public static void main(String[] args){
+
+        int n = 10;
+
+        String result = n%2==0 ? "even" : "odd";
+
+        System.out.println(result);
+
+    }
+}
