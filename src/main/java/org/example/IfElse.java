ca5ee2d1922257eb38b720add5d4713211cc47b0
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-07-28 13:02:30 +0530

    Revision of If else and making notes

---
+++ src/main/java/org/example/IfElse.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        int x = 18;
+        int y = 9;
+
+        if (x > y) System.out.println(x);
+        else System.out.println(y);
+
+    }
+}
