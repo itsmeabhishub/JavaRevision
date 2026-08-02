af19f9491e3704fcbf3b3b73339a484ea210bea9
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-08-02 18:22:10 +0530

    Revision on string

---
+++ src/main/java/org/example/DemoStr.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class DemoStr {
+    public static void main(String[] args) {
+//        String name1 = new String("Abhi"); never initilze like this
+
+        String name1 = "Abhi";
+        System.out.println("Hello " + name1);
+
+
+    }
+}
