af217cac207c56cc177ac24cd4ad5a08576808b6
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-08-27 23:52:02 +0530

    Exception handling in java

---
+++ src/main/java/ExceptionJava/ExceptionJ.java
@@ -0,0 +1,17 @@
+package ExceptionJava;
+
+public class ExceptionJ {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+
+        try {
+            j=18/i;
+        } catch (Exception e){
+            System.out.println("Something went wrong..");
+        }
+
+        System.out.println(j);
+    }
+}
