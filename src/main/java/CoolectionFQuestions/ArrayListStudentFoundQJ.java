4700dd8e6f6d782dbd1fa42765827192d9d852e3
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-09-18 00:12:12 +0530

    Question -minor update in find student question in java

--- src/main/java/CoolectionFQuestions/ArrayListStudentFoundQJ.java
+++ src/main/java/CoolectionFQuestions/ArrayListStudentFoundQJ.java
@@ -1,14 +1,14 @@
 package CoolectionFQuestions;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-class Student1{
+class Student1 {
     String name;
     int rollNo;
     int marks;
-    public Student1(String name, int rollNo, int marks){
+
+    public Student1(String name, int rollNo, int marks) {
         this.name = name;
         this.rollNo = rollNo;
         this.marks = marks;
@@ -17,15 +17,23 @@ class Student1{
 
 public class ArrayListStudentFoundQJ {
     public static void main(String[] args) {
-
         List<Student1> stdFound = new ArrayList<>();
         stdFound.add(new Student1("Abhishek", 1, 97));
-        stdFound.add(new Student1("Manas", 1, 87));
-        stdFound.add(new Student1("Ankit", 1, 97));
-        stdFound.add(new Student1("Abhay", 1, 77));
-        stdFound.add(new Student1("Anuj", 1, 67));
-            int findRoll = 103;
-            stdFound.contains(103);
-
+        stdFound.add(new Student1("Manas", 2, 87));
+        stdFound.add(new Student1("Ankit", 3, 97));
+        stdFound.add(new Student1("Abhay", 4, 77));
+        stdFound.add(new Student1("Anuj", 5, 67));
+        int findRoll = 1;
+        boolean found = false;
+        for (Student1 s : stdFound) {
+            if (findRoll == s.rollNo) {
+                System.out.println("Student found " + s.name);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("not found");
+        }
     }
 }
