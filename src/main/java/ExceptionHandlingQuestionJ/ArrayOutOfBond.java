622dfa62a95cb6c8ac0e60949a9a9afad09d40f7
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-09-18 23:33:38 +0530

    Question Exception Hnadling- Array out of bond exception in java

---
+++ src/main/java/ExceptionHandlingQuestionJ/ArrayOutOfBond.java
@@ -0,0 +1,13 @@
+package ExceptionHandlingQuestionJ;
+
+public class ArrayOutOfBond {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5};
+
+        try {
+            System.out.println(arr[6]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array out of Bond");
+        }
+    }
+}
